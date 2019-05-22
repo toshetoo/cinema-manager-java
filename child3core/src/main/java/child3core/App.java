17d81d5746cb637de1b400a54939c4e0ce38c7a8
Todor Ivanov <todor.ivanov@primeholding.com>
2019-05-22 19:27:57 +0300

    Add empty App class

---
+++ child3core/src/main/java/child3core/App.java
@@ -0,0 +1,14 @@
+package child3core;
+
+/**
+ * Hello world!
+ *
+ */
+public class App
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
+
